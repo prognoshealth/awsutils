e53b32fd93938801a581067ed2da1d77c7d9a208
caghyarian1995 <65416711+caghyarian1995@users.noreply.github.com>
2021-09-17 12:19:43 -0400

    Make s3EventRecordFromSNSWrapped public
    This is a useful function that should be available to users of the library.

--- s3eventutils/from_sns_event_test.go
+++ s3eventutils/from_sns_event_test.go
@@ -27,43 +27,43 @@ func createSNSRecord(message string) events.SNSEventRecord {
 	return snsEventRecord
 }
 
-func Test_s3EventRecordFromSNSWrapper(t *testing.T) {
+func Test_S3EventRecordFromSNSWrapper(t *testing.T) {
 	b, err := ioutil.ReadFile("testdata/valid_message_s3.json")
 	assert.NoError(t, err)
 
 	snsEvent := createSNSEvent(createSNSRecord(string(b)))
 
-	r, err := s3EventRecordFromSNSWrapper(snsEvent)
+	r, err := S3EventRecordFromSNSWrapper(snsEvent)
 
 	assert.NoError(t, err)
 	assert.Equal(t, "bktname", r.S3.Bucket.Name)
 	assert.Equal(t, "some/file/in/s3.txt", r.S3.Object.Key)
 }
 
-func Test_s3EventRecordFromSNSWrapper_error_sns_record_count(t *testing.T) {
+func Test_S3EventRecordFromSNSWrapper_error_sns_record_count(t *testing.T) {
 	b, err := ioutil.ReadFile("testdata/valid_message_s3.json")
 	assert.NoError(t, err)
 
 	snsEvent := createSNSEvent(createSNSRecord(string(b)), createSNSRecord(string(b)))
 
-	_, err = s3EventRecordFromSNSWrapper(snsEvent)
+	_, err = S3EventRecordFromSNSWrapper(snsEvent)
 	assert.Error(t, err)
 }
 
-func Test_s3EventRecordFromSNSWrapper_error_invalid_message(t *testing.T) {
+func Test_S3EventRecordFromSNSWrapper_error_invalid_message(t *testing.T) {
 	snsEvent := createSNSEvent(createSNSRecord("not json"))
 
-	_, err := s3EventRecordFromSNSWrapper(snsEvent)
+	_, err := S3EventRecordFromSNSWrapper(snsEvent)
 	assert.Error(t, err)
 }
 
-func Test_s3EventRecordFromSNSWrapper_error_s3_Record_count(t *testing.T) {
+func Test_S3EventRecordFromSNSWrapper_error_s3_Record_count(t *testing.T) {
 	b, err := ioutil.ReadFile("testdata/invalid_message_s3_count.json")
 	assert.NoError(t, err)
 
 	snsEvent := createSNSEvent(createSNSRecord(string(b)))
 
-	_, err = s3EventRecordFromSNSWrapper(snsEvent)
+	_, err = S3EventRecordFromSNSWrapper(snsEvent)
 	assert.Error(t, err)
 }
 
